2659ac8a18f63958303818e0e01d0d564a50e72c
PiyaSan <ahiresharanya@gmail.com>
2024-09-29 20:07:00 +0530

    Delete alarms.py

--- alarms.py
+++
@@ -1,6 +0,0 @@
-import time
-
-def set_alarm(minutes, email):
-    print(f"Alarm set for {minutes} minutes for {email}.")
-    time.sleep(minutes * 60)
-    print(f"Insulin Dose Alarm: {email}, time to take your insulin dose.")
